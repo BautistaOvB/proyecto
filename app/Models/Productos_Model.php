127f1d822abbf3275bc755201f8165666093d76c
BautistaOvB <ovallebravobautista@gmail.com>
2025-06-07 12:36:54 -0300

    actualizacion

--- app/Models/Productos_Model.php
+++ app/Models/Productos_Model.php
@@ -6,10 +6,31 @@ class Productos_Model extends Model
 {
     protected $table = 'productos';
     protected $primaryKey = 'id_prod';
-    protected $allowedFields = ['id_prod', 'nombre', 'descripcion', 'precio', 'imagen', 'categoria'];
+    protected $allowedFields = ['id_prod', 'nombre', 'imagen', 'categoria_id', 'precio', "precio_vta", "stock", "stock_min", "eliminado"];
     
-    public function getProductos()
+    public function getBuilderProductos()
     {
-        return $this->findAll();
+        $db = \Config\Database::connect();
+        $builder = $db->table('productos');
+        $builder->select('*');
+        $builder->join('categorias', 'categorias.id = productos.categoria_id' );
+
+        return $builder;
+    }
+
+    public function getProductos($id =null)
+    {
+        $builder = $this->getBuilderProductos();
+        $builder->where('productos.id', $id);
+        $query = $builder->get();
+        return $query->getRowArray();
+    }
+
+    public function updateStock($id = null, $stock_actual = null)
+    {
+        $builder = $this->getBuilderProductos();
+        $builder->where('productos-id', $id);
+        $builder->set('productos.stock', $stock_actual);
+        $builder->update();
     }
 }
--- app/Views/Register.php
+++ app/Views/Register.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Comercialización | Tatu</title>
+    <title>Registro | Tatu</title>
     <link rel="stylesheet" href= "<?php echo base_url("assets/css/estilo.css"); ?>">
     <link rel="stylesheet" href="assets/css/contacto.css">
 </head>
